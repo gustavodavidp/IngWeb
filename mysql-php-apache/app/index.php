f35e8df975135b4e6a0019c18c573f8e5d3da714
gustavodavidp <gustavo.pol21@gmail.com>
2019-11-19 20:05:19 -0500

    Conectando la base de datos

--- mysql-php-apache/app/index.php
+++ mysql-php-apache/app/index.php
@@ -1,38 +1,29 @@
 
-<?php $metaTags = 'tag1 tag2'; ?>
+
 <?php
 
+
+
+
+
+require_once("Db/db.php"); //Se incluye el archivo que contiene la conexión a la BD
 //Verificando controladores
 // la variable controller guarda el nombre del controlador y action guarda la acción por ejemplo registrar 
 //si la variable controller y action son pasadas por la url desde layout.php entran en el if
-if (isset($_GET['controller'])&&isset($_GET['action'])) {
-	$controller=$_GET['controller'];
-	$action=$_GET['action'];
-} else {
-	$controller='usuario';
-	$action=index;
-}	
+
+if (isset($_GET['controller']) && isset($_GET['action'])) { // Si hay un controlador y acción (método) definido...
+    $controller = $_GET['controller'];
+    $action = $_GET['action'];
+} else { //Si no se ha definido un controlador y acción (caso cuando se accede la primera vez en la sesión)
+    $controller = 'Home';
+    $action = 'index';
+}
+
+
 //carga la vista layout.php
 require_once('views/layouts/layout.php');
 
 
 
-$conn = new mysqli("mysql", "root", "clave123", "mi_db");
-// Verifica conexión
-if ($conn->connect_error) {
-	die("La conexión falló: " . $conn->connect_error);
-}else echo "Conexión exitosa";
 
-$sql = "SELECT nombre FROM usuarios";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-	// Datos de salida de cada registro
-	while($row = $result->fetch_assoc()) {
-		echo $row['nombre']."<br>";
-	}
-} else {
-	echo "Ningún resultado para la tabla usuarios";
-}
-$conn->close();
 ?>
